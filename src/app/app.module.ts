d3f8d0c582b849118ec702ef70855c35b40bf129
15138730370 <1037483576@qq.com>
2018-02-24 16:46:39 +0800

    save to update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IonicModule } from './ionic/index';
+import { We7RouterModule } from 'meepo-we7-router';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(AppComponent)
+    IonicModule.forRoot(AppComponent),
+    We7RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
